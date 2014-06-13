6172c9e1902499f05d1af6adf2c354745d36d5b1
Marcos López <marcos.lopez@sanitatintegral.org>
2014-06-13 11:58:49 +0200

    All project have been modified with autopep8 script

--- djiki/auth/base.py
+++ djiki/auth/base.py
@@ -13,34 +13,41 @@ These methods must accept the following arguments:
 The methods must return a ``bool`` value.
 """
 
+
 class UnrestrictedAccess(object):
-	"Everyone is permitted to do anything."
-	def can_view(self, request, target):
-		return True
 
-	def can_create(self, request, target):
-		return True
+    "Everyone is permitted to do anything."
 
-	def can_edit(self, request, target):
-		return True
+    def can_view(self, request, target):
+        return True
 
-	def can_view_history(self, request, target):
-		return True
+    def can_create(self, request, target):
+        return True
+
+    def can_edit(self, request, target):
+        return True
+
+    def can_view_history(self, request, target):
+        return True
 
 
 class OnlyAuthenticatedEdits(UnrestrictedAccess):
-	"Only authenticated users can modify the contents."
-	def can_create(self, request, target):
-		return request.user.is_authenticated()
 
-	def can_edit(self, request, target):
-		return request.user.is_authenticated()
+    "Only authenticated users can modify the contents."
+
+    def can_create(self, request, target):
+        return request.user.is_authenticated()
+
+    def can_edit(self, request, target):
+        return request.user.is_authenticated()
 
 
 class OnlyAdminEdits(UnrestrictedAccess):
-	"Only admin users can modify the contents."
-	def can_create(self, request, target):
-		return request.user.is_authenticated() and request.user.is_superuser
 
-	def can_edit(self, request, target):
-		return request.user.is_authenticated() and request.user.is_superuser
+    "Only admin users can modify the contents."
+
+    def can_create(self, request, target):
+        return request.user.is_authenticated() and request.user.is_superuser
+
+    def can_edit(self, request, target):
+        return request.user.is_authenticated() and request.user.is_superuser
